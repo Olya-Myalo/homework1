40e3092e87e96b7d7e17e049c3b0d2d8dd145e4e
Olya-Myalo <olyamyalo@mail.ru>
2023-04-09 14:06:13 +0300

    completed hw2

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 // let a = '20'; //1
 // alert (a); //
 
-const year = Number (2007);
-alert( Первый iphone вышел в продажу ${year} года );
+//const year = Number (2007);//2
+//alert('Первый iphone вышел в продажу 2007 года');//
 
+//const creator = String('Брендан Айк');//3
+//alert ('Создатель Java Script является Брендан Айк')//
+
+//let x = 10;//4
+//let y = 2;//
+//alert(x+y);//
+//alert(x-y);//
+//alert(x*y);//
+//alert(x/y);//
+
+//let result = 2**5;//5
+//alert(result);//
+
+//let a = 9;//6
+//let b = 2;//
+//alert(a/b);//
+
+//let num = 1;//7
+//num = num + 5;//
+//num = num - 3;//
+//num = num * 7;//
+//num = num / 3;//
+//num = num + 1;//
+//num = num - 1;//
+//alert(num);//
+
+//let year = Number(prompt('Сколько Вам лет?'))//8
+//alert('Вам '+ year + ' лет!')//
+
+//let user = { name: "John", years: 30 };//9
+// let {name, years: age, isAdmin = false} = user;//
+  //alert( name ); // Olya//
+  //alert( age ); // 30//
+  //alert( isAdmin ); // false//
+//let info = prompt("Какую информацию хотите узнать о пользователе?", "name");//
+
+let name = prompt('Как Вас зовут?');
+alert('Привет ' + name + '!')
