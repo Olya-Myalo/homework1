b7e2cdbc8819ca536f476863790279153983acd1
Olya-Myalo <olyamyalo@mail.ru>
2023-04-12 14:34:34 +0300

    three tasks have been completed

--- script.js
+++ script.js
@@ -49,3 +49,34 @@
 
 //let name = prompt('Как Вас зовут?');//
 //alert('Привет ' + name + '!')//
+
+
+                        //HOMEWORK 3//
+//let password = String(prompt('Введите пароль'));//1
+
+//if (password == 'пароль') {
+//  alert ('Пароль введён верно');
+//} else {
+ // alert ('Пароль введён не верно');
+//}
+
+//let num = 3;//2
+//let num = 0;
+//let num = 10;
+//let num = - 3;
+//let num = 2;
+//if (num > 0 && num < 10) {
+//	console.log ('Верно');
+//} else {
+//	console.log ('Неверно');
+//}
+
+//let d = 5;//3
+//let e = 175;
+
+//if (d > 100 || e > 100) {
+ // console.log ('Верно');
+//} else {
+ // console.log ('Неверно');
+//}
+
